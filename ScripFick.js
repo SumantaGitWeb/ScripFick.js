2f489fff8f38d8ae92044e6d6734938a7d950729
Sumanta Kumar Adhek <88125165+SumantaGitWeb@users.noreply.github.com>
2022-01-13 11:46:06 +0530

    Add files via upload

---
+++ ScripFick.js
@@ -0,0 +1,17 @@
+// Basic Functions
+
+function log(_log){
+    console.log(_log);
+};
+
+function caut(_caut){
+    alert(_caut);
+};
+
+function conf(_conf){
+    confirm(_conf);
+};
+
+function ask(_ask){
+    prompt(_ask);
+};
